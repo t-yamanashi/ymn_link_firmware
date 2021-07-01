a0dcd2bbe4f29b0fd33c61704ad5db3f31ff89bd
user <user@penguin>
2021-07-01 16:45:09 +0900

    update

--- ymn_link_firmware.ino
+++ ymn_link_firmware.ino
@@ -1,28 +1,72 @@
+String gcmd;
+
+/*
+ * Initialize the serial port and digital port
+ */
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  for (int port = 2; port <= 13; port++) {
+    pinMode(port, OUTPUT);
+  }
   Serial.begin(115200);
+  gcmd = "";
 }
 
+/*
+ * Use a semicolon as a delimiter
+ * And execute the command
+ */
 void loop() {
   if (!Serial.available()) {
     return;
   }
-  char cmd = Serial.read();
+  
+  char data = Serial.read();
+  if(data != ';') {
+    gcmd += data;
+    return;
+  }
+  
+  exec(gcmd);
+  gcmd = "";
+}
 
-  if (cmd == '0' || cmd == '1') {
-    led(cmd);
-  } else if (cmd == 'a') {
-    getdata();
+/*
+ * Parse and execute commands
+ */
+void exec(String cmd) {
+  String sec = cmd.substring(0, 1);
+  String portstr = cmd.substring(1);
+  int port = portstr.toInt();
+
+  if (sec == "1") {
+    setdata(port, 1);
+  } else if (sec == "0") {
+    setdata(port, 0);
+  } else if (sec == "a") {
+    getdata(port);
   }
 }
 
-void led(char cmd) {
-  int output = cmd == '1' ? HIGH : LOW;
-  digitalWrite(LED_BUILTIN, output);
-  Serial.print("OK;");
+/*
+ * Digital output
+ */
+void setdata(int port, int val) {
+  if (port < 2) {
+    Serial.print("NG");
+    return;
+  } 
+  digitalWrite(port, val);
+  Serial.print("OK");
 }
 
-void getdata() {
-  String data = String(analogRead(0), DEC);
-  Serial.print(data + ";");
+/*
+ * Get analog data
+ */
+void getdata(int port) {
+  if (port > 5) {
+    Serial.print("NG");
+    return;
+  } 
+  String data = String(analogRead(port), DEC);
+  Serial.print(data);
 }
